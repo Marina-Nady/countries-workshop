b2ef8b84eb475af0882d93c8445757af1d2c0e76
Marina Nady <mnady@turndigital.net>
2023-05-05 01:16:12 +0300

    styling details page

--- src/components/DetailsPage.vue
+++ src/components/DetailsPage.vue
@@ -1,58 +1,69 @@
 <template>
 <div class="details-page" v-for="(item,index) in details" :key="index">
-  <button>Back</button>
-  <div class="details-page__image">
-    <img :src="item.flags.png">
+  <div class="details-page__back-btn">
+  <router-link to="/">
+    <button>
+      <span class="icon-left-arrow-long details-page__back-btn__btn-icon"></span>
+      <span class="details-page__back-btn__btn-text">Back</span>
+    </button>
+  </router-link>
   </div>
-  <div class="details-page__info">
-    <h3 class="details-page__info__title">{{item.name.common}}</h3>
-    <div class="details-page__info__content">
-      <div class="details-page__info__first-col">
-        <div>
-          <label>Native Name: </label>
-          <span>{{item.name.common}}</span>
-        </div>
-        <div>
-          <label>Population: </label>
-          <span>{{item.population}}</span>
-        </div>
-        <div>
-          <label>Region: </label>
-          <span>{{item.region}}</span>
-        </div>
-        <div>
-          <label>Sub Region: </label>
-          <span>{{item.subregion}}</span>
-        </div>
-        <div>
-          <label>Capital: </label>
-          <span v-for="(item,index) in item.capital" :key="index">{{item}}</span>
-        </div>
-      </div>
-      <div class="details-page__info__second-col">
-        <div>
-          <label>Top Level Domain: </label>
-          <span>{{item.tld[0]}}</span>
-        </div>
-        <div>
-          <label>Currencies: </label>
-          <span v-for="(item,index) in item.currencies" :key="index">{{item.name}}</span>
-        </div>
-        <div>
-          <label>Languages: </label>
-          <span v-for="(item,index) in item.languages" :key="index">{{item.name}},</span>
-        </div>
+  <div class="details-page__content">
+    <div class="details-page__image-container">
+      <div class="details-page__image">
+        <img :src="item.flags.svg">
       </div>
     </div>
-    <div class="details-page__info__borders">
-      <label>Border Countries</label>
-      <button v-for="(item,index) in item.borders" 
-              :key="index" 
-              :value="item" 
-              @click="openBorderCounrty">{{item}}</button>
-      
-    </div>
+    <div class="details-page__info">
+      <h3 class="details-page__info__title">{{item.name.common}}</h3>
+      <div class="details-page__info__content">
+        <div class="details-page__info__first-col">
+          <div class="details-page__info__row">
+            <label class="details-page__info__label">Native Name: </label>
+            <span>{{item.name.common}}</span>
+          </div>
+          <div class="details-page__info__row">
+            <label class="details-page__info__label">Population: </label>
+            <span>{{item.population}}</span>
+          </div>
+          <div class="details-page__info__row">
+            <label class="details-page__info__label">Region: </label>
+            <span>{{item.region}}</span>
+          </div>
+          <div class="details-page__info__row">
+            <label class="details-page__info__label">Sub Region: </label>
+            <span>{{item.subregion}}</span>
+          </div>
+          <div class="details-page__info__row">
+            <label class="details-page__info__label">Capital: </label>
+            <span v-for="(item,index) in item.capital" :key="index">{{item}}</span>
+          </div>
+        </div>
+        <div class="details-page__info__second-col">
+          <div class="details-page__info__row">
+            <label class="details-page__info__label">Top Level Domain: </label>
+            <span>{{item.tld[0]}}</span>
+          </div>
+          <div class="details-page__info__row">
+            <label class="details-page__info__label">Currencies: </label>
+            <span v-for="(item,index) in item.currencies" :key="index">{{item.name}}</span>
+          </div>
+          <div class="details-page__info__row">
+            <label class="details-page__info__label">Languages: </label>
+            <span v-for="(item,index) in item.languages" :key="index">{{item}}<span>,</span></span>
+          </div>
+        </div>
+      </div>
+      <div class="details-page__info__borders details-page__info__row">
+        <label class="details-page__info__label">Border Countries:</label>
+        <button v-for="(item,index) in item.borders" 
+                :key="index" 
+                :value="item" 
+                @click="openBorderCountry">{{item}}</button>
+        
+      </div>
 
+    </div>
   </div>
 
 </div>
@@ -67,6 +78,7 @@ export default {
         return{
           details: null,
           name: this.$attrs.name,
+          borders: null
         }
     },
     methods:{
@@ -88,7 +100,7 @@ export default {
                           .catch(err => {
                               return err})
       },
-      openBorderCounrty(e){
+      openBorderCountry(e){
         let code = e.target.value
         this.getBorder(code)
 
@@ -96,7 +108,9 @@ export default {
     },
     mounted(){
        this.getDetails()
-    }
+
+    },
+
 
 
 }
