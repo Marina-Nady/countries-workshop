5df634a02cd4008780a5e3c289b1b3b586c14b49
Marina Nady <mnady@turndigital.net>
2023-05-14 13:45:08 +0300

    change history mode

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 
 import { createApp } from 'vue'
-import {createRouter,createWebHistory} from 'vue-router'
+import {createRouter,createWebHashHistory} from 'vue-router'
 import App from './App.vue'
 import Home from './components/Home.vue'
 import DetailsPage from './components/DetailsPage.vue'
@@ -21,8 +21,7 @@ const routes = [
 ]
 
 const router =  createRouter({
-    history: createWebHistory(),
-    mode: 'history',
+    history: createWebHashHistory(),
     routes,
 
     
